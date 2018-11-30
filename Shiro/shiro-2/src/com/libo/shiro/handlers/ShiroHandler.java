d14eb95b3f901e39d790d3afa162f6fdfa62e1d9
bestrow <1142465640@qq.com>
2018-11-30 23:16:31 +0800

    shiro学习

---
+++ Shiro/shiro-2/src/com/libo/shiro/handlers/ShiroHandler.java
@@ -0,0 +1,29 @@
+package com.libo.shiro.handlers;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/shiro")
+public class ShiroHandler {
+
+	@RequestMapping("/login")
+	public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+		Subject currentUser = SecurityUtils.getSubject();
+		if (!currentUser.isAuthenticated()) {
+			UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+			token.setRememberMe(true);
+			try {
+				currentUser.login(token);
+			} catch (AuthenticationException e) {
+				System.out.println("µÇÂ¼Ê§°Ü£º"+e.getMessage());
+			}
+		}
+		return "redirect:/list.jsp";
+	}
+}
