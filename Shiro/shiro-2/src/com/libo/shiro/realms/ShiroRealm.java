c811563ec0e83ee02d676d0ceac7fe834b69cd2e
bestrow <1142465640@qq.com>
2018-12-05 22:30:09 +0800

    实现授权realm

--- Shiro/shiro-2/src/com/libo/shiro/realms/ShiroRealm.java
+++ Shiro/shiro-2/src/com/libo/shiro/realms/ShiroRealm.java
@@ -1,5 +1,8 @@
 package com.libo.shiro.realms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -7,11 +10,15 @@ import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.crypto.hash.SimpleHash;
-import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 
-public class ShiroRealm extends AuthenticatingRealm{
+import sun.launcher.resources.launcher;
+
+public class ShiroRealm extends AuthorizingRealm{
 
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
@@ -55,6 +62,26 @@ public class ShiroRealm extends AuthenticatingRealm{
 		
 		SimpleAuthenticationInfo info = null; // new SimpleAuthenticationInfo(principal , credentials , realmName);
 		info = new SimpleAuthenticationInfo(principal, credentials, credentialsSalt, realmName);
+		return info;
+	}
+
+	// 授权会被shiro 回调的方法
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		 // 1. 从PrincipalCollection 中来获取登录用户的信息
+		Object principal = principals.getPrimaryPrincipal();
+		
+		// 2. 利用登录的用户的信息来获取当前用户的角色或权限(可能需要查询数据库)
+		Set<String> roles = new HashSet<>();
+		roles.add("user");
+		if ("admin".equals(principal)) {
+			roles.add("admin");
+		}
+		
+		// 3. 创建 SimpleAuthorizationInfo, 并设置其roles属性
+		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo(roles);
+		
+		// 4. 返回 SimpleAuthorizationInfo 对象
 		return info;
 	}
 
