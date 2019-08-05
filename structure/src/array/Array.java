e7e573ac6f942ada7805ba01759df5a3ddd35bc6
bestrow <1142465640@qq.com>
2019-08-05 19:55:18 +0800

    修改array

--- structure/src/array/Array.java
+++ structure/src/array/Array.java
@@ -1,7 +1,8 @@
-package array;
+import org.omg.CORBA.Object;
+
+import java.util.Arrays;
 
 public class Array<E> {
-
     private E[] data;
     private int size;
 
@@ -27,45 +28,114 @@ public class Array<E> {
     }
 
     public void addLast(E e) {
-        if (size == data.length) {
-            throw new IllegalArgumentException("AddLast failed. array.Array is full.");
+        add(size, e);
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Require index>=0 and index<=size.");
         }
-        data[size] = e;
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
         size++;
     }
 
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed.Index is illegal.");
+        }
+        data[index] = e;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed.Index is illegal.");
+        }
+        return data[index];
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public E remove(int index) {
-        E ret;
-        ret = data[index];
-        for (int i = index; i < size-1; i++) {
-            data[i] = data[i + 1];
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("remove failed.Index is illegal.");
+        }
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
         }
         size--;
         data[size] = null;
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
         return ret;
-    }
-
-    public E removeLast() {
-        return remove(size - 1);
     }
 
     public E removeFirst() {
         return remove(0);
     }
 
-    //======================
-    public E get(int index) {
-        return data[index];
+    public E removeLast() {
+        return remove(size - 1);
     }
 
-    public E getLast() {
-        return get(size - 1);
+    public boolean removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            E ret = remove(index);
+            if (ret.equals(e)) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public E getFirst() {
-        return get(0);
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d ,capacity = %d\n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(",");
+            }
+        }
+        res.append(']');
+        return res.toString();
     }
 
-
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
 }
